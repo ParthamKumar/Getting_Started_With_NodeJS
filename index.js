cb5e51777a85ced0bd9b2029f7bba6c798cff20d
Partham Kumar <parthamchawla@gmail.com>
2024-07-26 12:55:44 +0500

    Web Server Using Node JS

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
-const fs = require('fs')
+const http = require("http");
+const fs = require("fs");
+const { url } = require("inspector");
 
-// const txt = fs.readFileSync('demo.txt','utf-8')
-// console.log(txt)
+const index = fs.readFileSync("index.html", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
+// Create a local server to receive data from
 
-const t1 = performance.now()
+const server = http.createServer((req, res) => {
+  console.log(req.url,url.method);
 
-fs.readFile('demo.txt','utf-8',(err,txt)=>{
-    console.log(txt)
-})
+    if(req.url.startsWith('/product')){
+     const id = req.url.split('/')[2]
+     const product = products.find(p=>p.id===(+id))
+     console.log(product)
+     res.setHeader("Content-Type", "text/html");
+      let modifiedIndex = index
+        .replace("**title**", product.title)
+        .replace("**url**", product.thumbnail)
+        .replace("**price**", product.price)
+        .replace("**rating**", product.rating)
+      res.end(modifiedIndex);
+      return;
+    }
+  //  "/product":
+  //     res.setHeader("Content-Type", "text/html");
+  //     let modifiedIndex = index
+  //       .replace("**title**", product.title)
+  //       .replace("**url**", product.thumbnail)
+  //       .replace("**price**", product.price)
+  //       .replace("**rating**", product.rating)
+  //     res.end(modifiedIndex);
+  //     break;
 
-const t2 = performance.now()
+  switch (req.url) {
+    case "/":
+      res.setHeader("Content-Type", "text/html");
+      res.end(index);
+      break;
 
-console.log("Time Diff",t2-t1)
+    case "/api":
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(data));
+      break;
+    
+
+    default:
+      res.writeHead(404);
+      res.end();
+  }
+
+  console.log("Server Started");
+  // res.setHeader("Dummy", "DummyValue");
+  // res.setHeader("Content-Type", "text/html");
+  // res.end(JSON.stringify(data))
+});
+
+server.listen(8080);
